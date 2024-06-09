012a03c4e59d216353bfbfe2c7b96254388fab9c
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-06-09 23:01:12 +0200

    Add files via upload

---
+++ Debug/ReadMe.txt
@@ -0,0 +1,13 @@
+Debug is only applicable to experienced gamers！
+If using it for the first time, do not modify ini！
+
+PPPwn_cpp：
+https://github.com/xfangfang/PPPwn_cpp
+https://nightly.link/xfangfang/PPPwn_cpp/workflows/ci.yaml/main?status=completed
+
+PPPwn_go：
+https://github.com/wetor/PPPwn_go
+
+Collection Table：
+https://docs.google.com/spreadsheets/d/1C8zo9YIl6pCx1B_k_7tt_spTWc-Glfs3UdV4WcubPdc/edit#gid=0
+
---
+++ PoC.bat
@@ -0,0 +1,16 @@
+@echo off 
+echo PSGO.eu.org
+echo.
+echo Loading pppwn ...
+echo.
+echo If a Python error is prompted please uninstall and reinstall Python/pip/scapy 
+echo.
+echo Now! no need to get PS4 into test Test Internet Connection
+echo.
+echo If you see done! when entering stage 4, it means success. 
+echo Otherwise, please close this CMD windows and turn off the PS4 network switch and then turn it on again and try again!
+echo.
+@echo off 
+python pppwn.py --interface="TP-LINK Gigabit Ethernet USB Adapter" --fw=1100 --stage1=PS4_stage_bin_all/PS4-11.00/stage1/stage1.bin --stage2=PS4_stage_bin_all/PS4-11.00/stage2/stage2.bin
+@echo off 
+pause
